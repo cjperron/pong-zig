1cc16d069f821f1ee08801d42da458e5114cd371
Alan Volker <volkeralan12@gmail.com>
2025-12-07 11:25:11 -0300

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const pong_zig = @import("pong_zig");
+
+pub fn main() !void {
+    // Prints to stderr, ignoring potential errors.
+    std.debug.print("Pong en LAN!");
+}
