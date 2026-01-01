4cf13b3a39689e4a984b3eba69dba4ab841324f7
Alan Volker <volkeralan12@gmail.com>
2026-01-01 02:05:14 -0300

    titulo pong que vuela hacia abajo

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,22 @@
 const std = @import("std");
-const pong_zig = @import("pong_zig");
 const rl = @import("raylib");
+const pong_zig = @import("pong_zig");
+
+const Widget = pong_zig.widgets.Widget;
+const Text = pong_zig.widgets.Text;
 
 var displayConfig: pong_zig.DisplayConfig = undefined;
+
+var screenWidth: i32 = undefined;
+var screenHeight: i32 = undefined;
 
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
     displayConfig = try pong_zig.DisplayConfig.load();
 
-    const screenWidth = displayConfig.res.width;
-    const screenHeight = displayConfig.res.height;
+    screenWidth = displayConfig.res.width;
+    screenHeight = displayConfig.res.height;
 
     rl.initWindow(screenWidth, screenHeight, "Pong - Menu Principal");
 
@@ -19,24 +25,63 @@ pub fn main() anyerror!void {
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+    // init allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    // init widgets
+    var main_menu_widgets: std.ArrayList(Widget) = try std.ArrayList(Widget).initCapacity(alloc, 16);
+    defer main_menu_widgets.deinit(alloc);
+
+
+    // widgets
+    const title_text = "PONG";
+    const title_font_size = 120;
+    const title_width = rl.measureText(title_text, title_font_size);
+    const title_x = @divTrunc(screenWidth - title_width, 2);
+    const title_y = 150;
+
+    const titulo: Widget = Widget.initUnderlinedText(.{ .inner_text = Text.init(.{
+        .text = title_text,
+        .x = title_x,
+        .y = title_y,
+        .font_size = title_font_size,
+        .color = rl.Color.white,
+        .update_condition = &struct {
+            pub fn update_c() bool {
+                return true;
+            }
+        }.update_c,
+        .update_action = &struct {
+            pub fn update_a(this: *Text) void {
+                this.*.y = @mod((this.*.y + 1), screenHeight - 120);
+            }
+        }.update_a,
+    })
+    // underline defaulted
+    });
+
+    try main_menu_widgets.append(alloc, titulo);
+
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
-        update_main_menu();
+        update_main_menu(main_menu_widgets.items);
         // Draw
-        draw_main_menu();
-
-
+        draw_main_menu(main_menu_widgets.items);
     }
 }
 
-fn update_main_menu() void {
-
+fn update_main_menu(w: []Widget) void {
+    for (w) |*widget| {
+        widget.update();
+    }
 }
 
-fn draw_main_menu() void {
+fn draw_main_menu(w: []Widget) void {
     const background_color = rl.Color{ .r = 20, .g = 20, .b = 30, .a = 255 };
-    const screenWidth = displayConfig.res.width;
+    // const screenWidth = displayConfig.res.width;
     // const screenHeight = displayConfig.res.height;
     // Draw
     //----------------------------------------------------------------------------------
@@ -46,20 +91,7 @@ fn draw_main_menu() void {
     // Fondo oscuro
     rl.clearBackground(background_color);
 
-    // Título "PONG" en grande
-    const title = "PONG";
-    const fontSize = 120;
-    const titleWidth = rl.measureText(title, fontSize);
-    const titleX = @divTrunc(screenWidth - titleWidth, 2);
-    const titleY = 150;
-
-    rl.drawText(title, titleX, titleY, fontSize, .white);
-
-    // Línea de subrayado
-    const offset = -10;
-    const lineY = titleY + fontSize + offset;
-    const lineWidth = titleWidth;
-    rl.drawRectangle(titleX, lineY, lineWidth, 5, .white);
-    //----------------------------------------------------------------------------------
-
+    for (w) |widget| {
+        widget.draw();
+    }
 }
--- src/widgets.zig
+++ src/widgets.zig
@@ -1,117 +1,211 @@
 const std = @import("std");
+
 const rl = @import("raylib");
 
+pub const Widget = union(enum) {
+    text: Text,
+    underlined_text: UnderlinedText,
+    button: Button,
+    menu_selection: MenuSelection,
+
+    pub fn initText(options: struct {
+        x: i32,
+        y: i32,
+        text: [:0]const u8,
+        font_size: i32 = 20,
+        color: rl.Color = rl.Color.white,
+        update_condition: ?*const fn () bool = null,
+        update_action: ?*const fn (*Text) void = null,
+    }) Widget {
+        return Widget{ .text = Text.init(.{
+            .x = options.x,
+            .y = options.y,
+            .text = options.text,
+            .font_size = options.font_size,
+            .color = options.color,
+            .update_condition = options.update_condition,
+            .update_action = options.update_action,
+        }) };
+    }
+
+    pub fn initUnderlinedText(options: struct {
+        inner_text: Text,
+        underline_size: i32 = 5,
+    }) Widget {
+        return Widget{ .underlined_text = UnderlinedText.init(.{ .inner_text = options.inner_text, .underline_size = options.underline_size }) };
+    }
+
+    pub fn initButton(options: struct {
+        label: [:0]const u8,
+        font_size: i32,
+        color: rl.Color,
+        bg_color: rl.Color,
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+        action: *const fn () void,
+    }) Widget {
+        return Widget{ .button = Button{
+            .label = options.label,
+            .font_size = options.font_size,
+            .color = options.color,
+            .bg_color = options.bg_color,
+            .x = options.x,
+            .y = options.y,
+            .width = options.width,
+            .height = options.height,
+            .action = options.action,
+        } };
+    }
+
+    pub fn initMenuSelection(options: struct {
+        buttons: []Button,
+        selected_index: usize = 0,
+    }) Widget {
+        return Widget{ .menu_selection = MenuSelection{
+            .buttons = options.buttons,
+            .selected_index = options.selected_index,
+        } };
+    }
+
+    pub fn draw(self: @This()) void {
+        switch (self) {
+            .text => |t| t.draw(),
+            .underlined_text => |ut| ut.draw(),
+            .button => |b| b.draw(),
+            .menu_selection => |ms| ms.draw(),
+        }
+    }
+
+    pub fn update(self: *@This()) void {
+        switch (self.*) {
+            .text => |*t| t.update(),
+            .underlined_text => |*ut| ut.update(),
+            .button => {},
+            .menu_selection => |*ms| ms.update(),
+        }
+    }
+};
+
 pub const Text = struct {
-	x: i32,
-	y: i32,
-	text: []const u8,
-	font_size: i32,
-	color: rl.Color,
-	updateable: bool,
+    x: i32,
+    y: i32,
+    text: [:0]const u8,
+    font_size: i32,
+    color: rl.Color,
+    // Logica de actualizacion
+    update_condition: ?*const fn () bool,
+    update_action: ?*const fn (*@This()) void,
 
-	// TODO: init()
+    pub fn init(
+        options: struct {
+            x: i32,
+            y: i32,
+            text: [:0]const u8,
+            font_size: i32 = 20,
+            color: rl.Color = rl.Color.white,
+            // En el caso que se tenga que actualizar el texto, hay que manejar con logica custom.
+            update_condition: ?*const fn () bool = null, // Si el texto se actualiza, esto NO es null
+            // Si actualizamos el widget, tenemos que sobrescribirlo.
+            // Naturalmente, para lograrlo de la forma mas clean, lo mas sencillo es modificarla por referencia, y darle el poder al usuario de decir que modificar.
+            update_action: ?*const fn (*Text) void = null,
+        },
+    ) Text {
+        return Text{ .x = options.x, .y = options.y, .text = options.text, .font_size = options.font_size, .color = options.color, .update_condition = options.update_condition, .update_action = options.update_action };
+    }
 
-	pub fn update(self: *Text) void {
-		// Placeholder for update logic if needed in the future
-		if (self.updateable) {
-			// Implement any dynamic update logic here
-		}
-	}
+    pub fn update(self: *@This()) void {
+        // Placeholder for update logic if needed in the future
+        if (self.update_condition) |condition| {
+            if (condition()) {
+                if (self.update_action) |action| {
+                    action(self);
+                }
+            }
+        }
+    }
 
-	pub fn draw(self: Text) void {
-		rl.drawText(self.text, self.x, self.y, self.font_size, self.color);
-	}
+    pub fn draw(self: @This()) void {
+        rl.drawText(self.text, self.x, self.y, self.font_size, self.color);
+    }
 };
 
 pub const UnderlinedText = struct {
-	text: []const u8,
-	font_size: i32,
-	color: rl.Color,
-	updateable: bool,
+    inner_text: Text,
+    underline_size: i32,
 
-	// TODO : init()
+    pub fn init(options: struct { inner_text: Text, underline_size: i32 = 5 }) UnderlinedText {
+        return UnderlinedText{ .inner_text = options.inner_text, .underline_size = options.underline_size };
+    }
 
-	pub fn update(self: *UnderlinedText) void {
-		// Placeholder for update logic if needed in the future
-		if (self.updateable) {
-			// Implement any dynamic update logic here
-		}
-	}
+    pub fn draw(self: UnderlinedText) void {
+        self.inner_text.draw();
+        const text_width = rl.measureText(self.inner_text.text, self.inner_text.font_size);
+        const offset = @max(2, @divTrunc(self.inner_text.font_size, 20)); // Muy cerca, proporcional
+        const line_y = self.inner_text.y + self.inner_text.font_size - @divTrunc(self.inner_text.font_size, 5) + offset;
+        rl.drawRectangle(self.inner_text.x, line_y, text_width, self.underline_size, self.inner_text.color);
+    }
 
-
-	pub fn draw(self: UnderlinedText, x: i32, y: i32) void {
-		// Draw the text
-		rl.drawText(self.text, x, y, self.font_size, self.color);
-
-		// Calculate the width of the text for underlining
-		const textWidth = rl.measureText(self.text, self.font_size);
-		const underlineY = y + self.font_size + 5; // 5 pixels below the text
-
-		// Draw the underline
-		rl.drawLine(x, underlineY, x + textWidth, underlineY, self.color);
-	}
+    pub fn update(self: *UnderlinedText) void {
+        self.inner_text.update();
+        // TODO: if inner_text updateable, update line width along with color.
+    }
 };
 
-
-
 pub const Button = struct {
-	label: []const u8,
-	font_size: i32,
-	color: rl.Color,
-	bg_color: rl.Color,
-	x: i32,
-	y: i32,
-	width: i32,
-	height: i32,
+    label: [:0]const u8,
+    font_size: i32,
+    color: rl.Color,
+    bg_color: rl.Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
 
-	action: fn() void,
+    action: *const fn () void,
 
-	// TODO: init()
+    pub fn isClicked(self: Button) bool {
+        const mouseX = rl.getMouseX();
+        const mouseY = rl.getMouseY();
+        const mousePressed = rl.isMouseButtonPressed(rl.MouseButton.left);
 
-	pub fn isClicked(self: Button) bool {
-		const mouseX = rl.getMouseX();
-		const mouseY = rl.getMouseY();
-		const mousePressed = rl.isMouseButtonPressed(rl.MOUSE_LEFT_BUTTON);
+        return mousePressed and
+            (mouseX >= self.x) and (mouseX <= self.x + self.width) and
+            (mouseY >= self.y) and (mouseY <= self.y + self.height);
+    }
 
-		return mousePressed and
-			(mouseX >= self.x) and (mouseX <= self.x + self.width) and
-			(mouseY >= self.y) and (mouseY <= self.y + self.height);
-	}
+    pub fn draw(self: Button) void {
+        // Draw button background
+        rl.drawRectangle(self.x, self.y, self.width, self.height, self.bg_color);
 
-	pub fn draw(self: Button) void {
-		// Draw button background
-		rl.drawRectangle(self.x, self.y, self.width, self.height, self.bg_color);
+        // Calculate text position to center it in the button
+        const textWidth = rl.measureText(self.label, self.font_size);
+        const textX = self.x + @divTrunc((self.width - textWidth), 2);
+        const textY = self.y + @divTrunc((self.height - self.font_size), 2);
 
-		// Calculate text position to center it in the button
-		const textWidth = rl.measureText(self.label, self.font_size);
-		const textX = self.x + (self.width - textWidth) / 2;
-		const textY = self.y + (self.height - self.font_size) / 2;
-
-		// Draw button label
-		rl.drawText(self.label, textX, textY, self.font_size, self.color);
-	}
+        // Draw button label
+        rl.drawText(self.label, textX, textY, self.font_size, self.color);
+    }
 };
 
 /// Ayudita para tener un array de botones con layout vertical u horizontal.
 pub const MenuSelection = struct {
-	buttons: []Button,
-	selected_index: usize,
+    buttons: []Button,
+    selected_index: usize,
 
+    pub fn update(self: *MenuSelection) void {
+        for (self.buttons, 0..) |*button, index| {
+            if (button.isClicked()) {
+                self.selected_index = index;
+                button.action();
+            }
+        }
+    }
 
-	// TODO: init()
-
-	pub fn update(self: *MenuSelection) void {
-		for (self.buttons, 0..) |*button, index| {
-			if (button.isClicked()) {
-				self.selected_index = index;
-				button.action();
-			}
-		}
-	}
-
-	pub fn draw(self: MenuSelection) void {
-		for (self.buttons) |button| {
-			button.draw();
-		}
-	}
+    pub fn draw(self: MenuSelection) void {
+        for (self.buttons) |button| {
+            button.draw();
+        }
+    }
 };
